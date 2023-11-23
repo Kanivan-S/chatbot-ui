4c5686fbc489fed87eb1f37543c84f0cb41306dc
Kanivan <skanivan12@gmail.com>
2023-11-24 00:57:54 +0530

    location lat and long

--- src/app/chat/fetchanswer.service.ts
+++ src/app/chat/fetchanswer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpErrorResponse } from '@angular/common/http';
+import { HttpClient,HttpErrorResponse,HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, of } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -7,12 +7,26 @@ import { ActivatedRoute, Router } from '@angular/router';
   providedIn: 'root'
 })
 export class FetchanswerService {
-  API:string="http://127.0.0.1:50979";
+  API:string="http://127.0.0.1:56469";
   constructor(private http:HttpClient,private router:Router,private route:ActivatedRoute) {}
-
-
   getAnswerForQs(form: FormData) {
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      },
+      (error) => {
+        console.error('Error getting location:', error);
+      }
+    );
+    const headers = new HttpHeaders({
+      "Lat":"10.592109",
+      "Long":"79.057093"
+    });
     return this.http.post<any>(`${this.API}/getAnswer`, form, {
+      headers: headers,
       observe: 'response',
     });
   }
